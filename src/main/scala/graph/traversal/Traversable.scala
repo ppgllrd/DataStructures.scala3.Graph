df1d8eaf77f682daee8c78b3a960441b57f4e757
ppgllrd <ppgllrd@gmail.com>
2023-02-18 19:07:07 +0100

    Comments.

--- src/main/scala/graph/traversal/Traversable.scala
+++ src/main/scala/graph/traversal/Traversable.scala
@@ -13,16 +13,36 @@ import scala.collection.Set
 trait Traversable[V]:
   def successors(vertex: V): Set[V]
 
+  /**
+   * Returns a depth first traversal starting from element `source`.
+   *
+   * @param source element where traversal should start.
+   * @return a depth first traversal starting from element `source`.
+   */
   def depthFirstTraversal(source: V): Traversal[V] =
     val dft = new DepthFirstTraversal(source, this)
     dft.traverse()
     dft
 
+  /**
+   * Returns a breadth first traversal starting from element `source`.
+   *
+   * @param source element where traversal should start.
+   * @return a breadth first traversal starting from element `source`.
+   */
   def breadthFirstTraversal(source: V): Traversal[V] =
     val bft = new BreadthFirstTraversal(source, this)
     bft.traverse()
     bft
 
+  /**
+   * Returns a traversal starting from element `_source`. The order in which elements are traversed depends on the
+   * policy implemented by the container passed as parameter.
+   *
+   * @param _source    element where traversal should start.
+   * @param _container container whose policy defines order in which elements are traversed.
+   * @return a traversal starting from element `_source`.
+   */
   def withContainerTraversal(_source: V, _container: Container[DirectedEdge[V]]): Traversal[V] =
     val _traversable = this
     val traversal = new Traversal[V]:
